710ad32982cc134a4f488c93ab9a25ccff21a6b5
Abdouramane SIDIBE <sidibeabdouramane@hotmail.fr>
2026-08-31 14:09:15 +0200

    tp2

--- src/main/java/fr/diginamic/hello/controleurs/HelloControleur.java
+++ src/main/java/fr/diginamic/hello/controleurs/HelloControleur.java
@@ -1,6 +1,7 @@
 package fr.diginamic.hello.controleurs;
 
 import fr.diginamic.hello.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloControleur {
-
+    @Autowired
     private final HelloService helloService;
 
     public HelloControleur(HelloService helloService) {
